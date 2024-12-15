4981622c4d56e512e748818c9e5412c989ee9597
John Drexler <johnrudolphdrexler@gmail.com>
2024-12-15 10:21:31 -0500

    WIP

--- resources/views/livewire/pre-game-lobby.blade.php
+++ resources/views/livewire/pre-game-lobby.blade.php
@@ -1,21 +1,4 @@
-<div 
-    x-init="
-        window.addEventListener('beforeunload', () => {
-            console.log('beforeunload');
-            $dispatch('disconnected');
-        });
-
-        document.addEventListener('visibilitychange', () => {
-            if (document.visibilityState === 'hidden') {
-                console.log('visibilitychange hidden');
-                $dispatch('disconnected');
-            }
-        });
-    "
-    wire:disconnected="$wire.handleDisconnect()"
-
-    wire:poll.10000ms="checkIfCanceled"
->
+<div wire:poll.10000ms="checkIfCanceled" class="mt-10">
     @if($this->game->players->count() === 1 && $this->player)
         <flux:card class="w-full">
             <flux:heading>Waiting for opponent</flux:heading>
